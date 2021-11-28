e5c4df6520000a68fbb1ced5c0b9deafed36e8a9
liamandaidan <liammclaughlinis@gmail.com>
2021-11-28 14:02:42 -0700

    First Commit Very wow, much work.

---
+++ src/java/servlets/ForgotServlet.java
@@ -0,0 +1,27 @@
+package servlets;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author liamm
+ */
+public class ForgotServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+}
---
+++ web/WEB-INF/forgot.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : forgot
+    Created on : 28-Nov-2021, 2:00:50 PM
+    Author     : liamm
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Forgot password</title>
+    </head>
+    <body>
+        <h1>Forgot Password</h1>
+    </body>
+</html>
